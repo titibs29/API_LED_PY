66bc91b92bffcb9ad5e11c6262441f1be52235ad
titibs29 <thibaut.rizzoli@hotmail.be>
2022-01-15 02:10:18 +0100

    added url_for and methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def hello_world():
 	return "html/index.html"
 
-@app.route("/<name>")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		return 'this is a post login'
+	else:
+		return 'this is a get login'
+
+@app.route("/user/<name>")
 def hello(name):
 	return f"Hello, {escape(name)}!"
+
+
+with app.test_request_context():
+	print(url_for('hello_world'))
+	print(url_for('hello', name="toi"))
+	print(url_for('login'))
